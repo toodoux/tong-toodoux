3380466e1e6046610ba482afb08456a8f01a2210
Maxime Alves <maxime.alves@openmailbox.org>
2015-09-07 11:29:06 +0200

    style update

--- javascripts/tong-toodoux.js
+++ javascripts/tong-toodoux.js
@@ -3,51 +3,322 @@ $(function() {
         title : "Tong Toodoux",
         subtitle : "Tong Toodoux",
         catchphrase : "La Tong d'Hiver",
+        author : "Alves Maxime",
+        company : "Cuir etc...",
+        facebook : "https://www.facebook.com/tongtoodoux",
+        twitter : "https://twitter.com/tongtoodoux",
+        github : "https://github.com/mrPain89/tong-toodoux/tree/gh-pages",
         button : {
-            text : "Choisir Ma Tong",
-            section : "attention"
-        }
-    },sections = [
+            text : {
+                fr : "Choisir Ma Tong",
+                en : "Choose My Flip-Flop",
+                ger : "Choisir Ma Tong"
+            },
+            section : "choose"
+        },
+        videos : [
+            "UntriedCorruptElver",
+            "IllegalMasculineBuffalo",
+            "DirectPracticalAlleycat",
+            "BrightDirectIchneumonfly"
+        ]
+    },meta = {
+        title : "La Tong D'Hiver Toodoux, par Pascal Nicolazo",
+        description : "Site de la Tong Toodoux, Tong habillée en cuir de vache naturelle, fabriquée en France",
+        tags : "Tongs, Tong, Toodoux, Tong-Toodoux, Chaussures, Hiver, Nicolazo, Pascal Nicolazo, Artisanat",
+        author : "Alves Maxime"
+    }, sections = [
         {
             id : "gallery",
-            name : "Gallerie",
-            subtitle : "les photos des Tongs Toodoux",
-            data : {
-            }
+            name : {
+                fr : "Galerie",
+                en : "Gallery",
+                ger : "Gallerie"
+            },
+            subtitle : {
+                fr : "les photos des Tongs Toodoux",
+                en : "pictures of the Toodoux flip-flops",
+                ger : "bilder aus dem Toodoux flip-flops"
+            }, 
+            items : [
+                {
+                    title : "Pour enfant",
+                    description : {
+                        fr : "Bleue, charolaise",
+                        en : "Bleue, charolaise",
+                        ger : "Bleue, charolaise"
+                    },
+                    category : "Enfant, 2015",
+                    date : "Septembre 2015",
+                    link : "https://i.imgur.com/6b75Hbd.jpg"
+
+                }, {
+                    title : "Blanches et pierre",
+                    description : {
+                        fr : "Bleue, charolaise",
+                        en : "Bleue, charolaise",
+                        ger : "Bleue, charolaise"
+                    },
+
+                    link : "http://i.imgur.com/uVo4jAE.jpg"
+                }, {
+                    title : "El Téide",
+                    description : {
+                        fr : "Bleue, charolaise",
+                        en : "Bleue, charolaise",
+                        ger : "Bleue, charolaise"
+                    },
+
+                    link : "http://i.imgur.com/sbvWmBx.jpg"
+                }, {
+                    title : "Automne",
+                    description : {
+                        fr : "Bleue, charolaise",
+                        en : "Bleue, charolaise",
+                        ger : "Bleue, charolaise"
+                    },
+
+                    link : "http://i.imgur.com/fh7sTpD.jpg"
+                }, {
+                    title : "Automne",
+                    description : {
+                        fr : "Bleue, charolaise",
+                        en : "Bleue, charolaise",
+                        ger : "Bleue, charolaise"
+                    },
+
+                    link : "http://i.imgur.com/9i5Gh3f.jpg"
+                }, {
+                    title : "Automne",
+                    description : {
+                        fr : "Bleue, charolaise",
+                        en : "Bleue, charolaise",
+                        ger : "Bleue, charolaise"
+                    },
+
+                    link : "http://i.imgur.com/LKClmuB.jpg"
+                }
+
+            ]
         },
         {
             id : "choose",
-            name : "Choisir",
-            subtitle : "ma Tong Personalisée",
-            data : {
-            }
+            name : {
+                fr : "Choisir",
+                en : "Choose",
+                ger : "Choisir"
+            },
+            subtitle : {
+                fr : "ma Tong Personalisée",
+                en : "my own Flip-Flops",
+                ger : "ma Tong Personalisée"
+            },
+            items : [
+                {
+                    title : {
+                        fr : "Taille",
+                        en : "Size",
+                        ger : "Taille2"
+                    },
+                    description : {
+                        fr : "ma pointure",
+                        en : "of my feet",
+                        ger : "vum die fuss"
+                    },
+                    icon : "http://i.imgur.com/bLGlfUu.png"
+                }, {
+                    title : {
+                         fr : "Couleur",
+                         en : "Color",
+                         ger : "Farbe"
+                    },
+                    description : {
+                        fr : "gers brides",
+                        en : "gers brides",
+                        ger : "des brides"
+                    },
+                    icon : "http://i.imgur.com/8zhn6Q1.png"
+                }, {
+                    title : {
+                        fr : "Peau",
+                        en : "Peau",
+                        ger : "Peau"
+                    },
+                    description : {
+                        fr : "pour habiller",
+                        en : "pour habiller",
+                        ger : "pour habiller"
+                    },
+                    icon : "http://i.imgur.com/l15mGeX.png"
+                }
+            ]
         },
         {
             id : "about",
-            name : "Les Liens",
-            subtitle : "vers notre réseau",
-            data : {
-            }
+            name : {
+                fr : "Les Liens",
+                en : "The Links",
+                ger : "Die Linke"
+            },
+            subtitle : {
+                fr : "vers notre réseau",
+                en : "vers notre réseau",
+                ger : "vers notre réseau"
+            },
+            items : [
+            /*
+                {
+                    name : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    subtitle : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    description : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    img : ""
+                },
+                {
+                    name : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    subtitle : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    description : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    img : ""
+                },
+                {
+                    name : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    subtitle : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    description : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    img : ""
+                },
+                {
+                    name : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    subtitle : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    description : {
+                        fr : "",
+                        en : "",
+                        de : ""
+                    },
+                    img : ""
+                }
+            */
+            ]
         },
         {
             id : "team",
-            name : "Le Créateur",
-            subtitle : "de la Tong Toodoux",
-            data : {
+            name : {
+                fr : "Le Créateur",
+                en : "The Creator",
+                ger : "Le Créateur"
+            },
+            subtitle : {
+                fr : "de la Tong Toodoux",
+                en : "of the Toodoux flip-flop",
+                ger : "de la Tong Toodoux"
+            },
+            text: {
+                fr : "",
+                en : "",
+                de : ""
+            },
+            item : {
+                name : "Pascal Nicolazo",
+                address : "16 rue des dominicains Guebwiler",
+                number : "06010010101",
+                email : "toodoux@laposte.net",
+                facebook : "https://www.facebook.com/pascal.nicolazo",
+                twitter : "",
+                linkedin : "",
+                role : {
+                    fr : "",
+                    en : "",
+                    de : ""
+                },
+                description : {
+                    fr : "",
+                    en : "",
+                    de : ""
+                },
+                img : "http://i.imgur.com/2ha7Gwv.jpg"
             }
         }
     ],
+     footer = {
+        company : "&copy;2015 - Cuir Etc...",
+        link : "https://www.facebook.com/pages/Animation-Cuir-Etc_"
+     },
+     headtemplate = _.template($('#head-template').html());
      navtemplate = _.template($('#nav-template').html()),
-     maintemplate = _.template($('#main-template').html());
-     footertemplate = _.template($('#footer-template').html());
+     maintemplate = _.template($('#main-template').html()),
+     footertemplate = _.template($('#footer-template').html()),
+     lang = $.cookie('lang') ? $.cookie('lang') : 'fr';
+    var gfyShow = function() {
+        $(".videos .gfyitem").attr("data-id",config.videos[Math.floor(Math.random() * config.videos.length)]);
+    };    
 
+    $('head').append(headtemplate({meta:meta}));
     $('nav').html(navtemplate({config:config, sections:sections}));
     $('#main').html(maintemplate({config:config, sections:sections}));
-    $('footer').html(footertemplate({config:config, sections:sections}));
+    $('footer').html(footertemplate({config:config, footer:footer}));
 
+    gfyShow();
+    
     $.each(sections, function(i, e) {
         var sectionstemplate = _.template($('#' + e.id + '-template').html());
         $('#' + e.id + ' .container').html(sectionstemplate({config:config, data:e}));
     });
 
+    /* Multi-lingual support */
+    $('#language').change(function() {
+        if ($(this).val() == lang)
+            return;
+
+        lang = $(this).val();
+        $.cookie('lang', lang);
+        window.location = '?lang=' + lang;    
+    });
+
+    /* Gallery slideshow */
+    $('.portfolio-link').click(function(e) {
+        e.preventDefault();
+        $(this).ekkoLightbox();
+    });
 });
